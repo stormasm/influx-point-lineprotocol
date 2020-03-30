9c33b3b94860c1be8055083d37b7cb4b37c6cfc4
Michael Angerman <stormasm@gmail.com>
2020-03-30 13:47:54 -0700

    initial commit of ipl0a

--- ipl0a/Readme.md
+++ ipl0a/Readme.md
@@ -1,5 +1,1 @@
-###### References
-
-https://facility9.com/2016/04/the-basics-of-rust-structs/
-
 https://users.rust-lang.org/t/cannot-move-out-of-x-which-is-behind-a-shared-reference/33263
---
+++ ipl0a/examples/ex00.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+struct Child {}
+
+#[derive(Default)]
+struct A {
+    map: HashMap<String, Child>,
+}
+
+impl A {
+    pub fn default() -> A {
+        let mut a = A {
+            map: HashMap::new(),
+        };
+        a.map.insert("one".to_string(), Child{});
+        a
+    }
+
+    pub fn get_map(&self) -> HashMap<String, Child> {
+        self.map
+    }
+}
+
+fn do_something(value: Child) {
+    println!("Value: {:?}", value);
+}
+
+fn main() {
+    let a = A::default();
+    let x = a.get_map();
+
+    for (key, value) in x {
+        println!("{}", key);
+        do_something(value);
+    }
+}
