ea6b0c78c17c68e136426db5b854d77f8406a666
Michael Angerman <stormasm@gmail.com>
2020-03-31 12:03:12 -0700

    get_lineprotocol is in there

--- csv0/src/address.rs
+++ csv0/src/address.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt::Write as FmtWrite;
 
 #[derive(Debug, Clone)]
 pub struct Address {
@@ -16,5 +17,24 @@ impl Address {
 
     pub fn p1(name: String, details: HashMap<String, String>) -> Address {
         Address { name, details }
+    }
+
+    pub fn get_lineprotocol(self) -> Result<String, Box<dyn std::error::Error>> {
+        let mut s = String::new();
+        write!(&mut s, "{},", self.name).expect("error in measurement");
+
+        for (key, val) in self.details {
+            write!(&mut s, "{}={},", key, val).expect("error in tagset");
+        }
+
+        // remove the last comma from the tagset
+        let strlen = s.len();
+        let mut s1 = String::from(s);
+        s1.remove(strlen - 1);
+
+        // add in a space between the tagset and the fieldset
+        write!(&mut s1, "{}", " ".to_string()).expect("error in space");
+
+        Ok(s1)
     }
 }
