9c1b2ac58052dc003545bc0f3777a060ea038b05
Michael Angerman <stormasm@gmail.com>
2020-03-30 18:07:22 -0700

    working

--- ipl0a/examples/ex01.rs
+++ ipl0a/examples/ex01.rs
@@ -1,4 +1,11 @@
-fn take_ownership(v1: &mut Vec<i32>) -> &Vec<i32>{
+fn take_ownership2(v2: &mut Vec<i32>) -> &Vec<i32> {
+    v2.push(50);
+    v2.push(51);
+    v2.push(52);
+    v2
+}
+
+fn take_ownership1(v1: &mut Vec<i32>) -> &Vec<i32> {
     v1.push(42);
     v1.push(43);
     v1.push(44);
@@ -7,6 +14,8 @@ fn take_ownership(v1: &mut Vec<i32>) -> &Vec<i32>{
 
 fn main() {
     let mut v1 = vec![1, 2, 3];
-    let x = take_ownership(&mut v1);
-    println!("{:?}",x);
+    let x = take_ownership1(&mut v1);
+    println!("{:?}", x);
+    let y = take_ownership2(&mut v1);
+    println!("{:?}", y);
 }
