0996f4e46c7f734c750aa334a7659cef69540e6d
Michael Angerman <stormasm@gmail.com>
2020-03-31 10:40:02 -0700

    p0

--- csv0/src/address.rs
+++ csv0/src/address.rs
@@ -7,7 +7,7 @@ pub struct Address {
 }
 
 impl Address {
-    pub fn new(name: String, state: String, zip: String) -> Address {
+    pub fn p0(name: String, state: String, zip: String) -> Address {
         let mut details = HashMap::new();
         details.insert("state".to_string(), state);
         details.insert("zip".to_string(), zip);
