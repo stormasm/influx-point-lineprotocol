623f50f06bb8b7fd728bd911565eef7c7d185eb8
Michael Angerman <stormasm@gmail.com>
2020-03-31 13:48:37 -0700

    mw

--- csv0/examples/address0a.rs
+++ csv0/examples/address0a.rs
@@ -1,11 +1,8 @@
 use ipl::address::Address;
 
 fn main() {
-    let t1 = Address::p0("michael".to_string(), "pa".to_string(), "15132".to_string());
-    let t2 = Address::p0("hb".to_string(), "pa".to_string(), "15132".to_string());
-    let t3 = Address::p0("iris".to_string(), "pa".to_string(), "15132".to_string());
-
-    let v1 = vec![t1, t2, t3];
+    let a1 = Address::p0("michael".to_string(), "pa".to_string(), "15132".to_string());
+    let v1 = vec![a1];
 
     for t in v1.iter() {
         println!("{:?}\n", t);
