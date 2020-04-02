e7079a4635251e139c7a7b195b0262b4b394b1d9
Michael Angerman <stormasm@gmail.com>
2020-04-01 21:56:05 -0700

    a method inside human called getlp is up and running as well

--- ipl0a/src/human.rs
+++ ipl0a/src/human.rs
@@ -9,4 +9,11 @@ impl Human {
     pub fn p1(details: HashMap<String, String>) -> Human {
         Human { details }
     }
+
+    pub fn getlp(self) -> String {
+        let mut s = String::new();
+        s.push_str(self.details.get("state").unwrap());
+        s.push_str(", country!");
+        s
+    }
 }
