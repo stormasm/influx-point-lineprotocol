d46f9e3c419cb25aea2897e81929f157df58f710
Michael Angerman <stormasm@gmail.com>
2020-04-03 10:45:58 -0700

    remove the directory csv0

--- csv0/examples/address0.rs
+++
@@ -1,8 +0,0 @@
-use ipl::address::Address;
-
-fn main() {
-    let t = Address::p0("michael".to_string(), "pa".to_string(), "15132".to_string());
-    println!("Name: {}", t.name);
-    println!("{:?}", t);
-    println!("{:?}", t.get_lineprotocol());
-}
--- csv0/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub use crate::point::Point;
-pub use crate::address::Address;
-pub mod point;
-pub mod address;
